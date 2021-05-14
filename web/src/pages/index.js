cce6eb04b0bfc56c976f8efef43172b427157929
Jelle <jelle@fashionunited.com>
2021-05-14 22:08:04 +0200

    updates

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -2,11 +2,28 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../containers/layout'
 
-const IndexPage = () => {
+export const homePageQuery = graphql`
+  query homePageQuery {
+    allSanityPage {
+      edges {
+        node {
+          heroTitle
+        }
+      }
+    }
+  }
+`
+
+const IndexPage = (props) => {
+  const { 
+      node: {
+        heroTitle,
+      }
+  } = props.data.allSanityPage.edges[0];
 
   return (
     <Layout>
-        <h1 hidden>Welcome to site</h1>
+        <h1>{heroTitle}</h1>
     </Layout>
   )
 }
