2e3cda1e7b8279569f8a01495a0d7312ce9727e1
Jelle <jelle@fashionunited.com>
2021-06-01 22:59:27 +0200

    WIP

--- web/src/pages/mijn-projecten.jsx
+++ web/src/pages/mijn-projecten.jsx
@@ -5,47 +5,55 @@ import Layout from '../containers/Layout';
 import Card from '../components/Card';
 
 export const projecPageQuery = graphql`
-  query MyQuery {
-    sanityProjects {
-      title
+  query projectPageQuery {
+    allSanityProjects {
+      edges {
+        node {
+          image1 {
+            asset {
+              gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, formats: JPG)
+            }
+          }
+          title
+          description
+        }
+      }
+    }
   }
-}
 `;
 
-const Cards = (props) => {
-  // const { edges } = data;
-  console.log(props.data);
+const MijnProjecten = ({ data }) => {
+  const { allSanityProjects: { edges } } = data;
   return (
-    <Grid container spacing={3}>
-      {/* {edges.map((node) => {
-        const {
-          title,
-          description,
-          image1: {
-            asset: {
-              gatsbyImageData: cardImage,
+    <Layout>
+      <h1>Mijn projecten</h1>
+      <Grid container spacing={3}>
+        {edges.map((card) => {
+          const {
+            node: {
+              title,
+              description,
+              image1: {
+                asset: {
+                  gatsbyImageData: cardImage,
+                },
+              },
             },
-          },
-        } = node;
-        return (
-          <Grid item lg={3} xs={12} md={4} sm={6}>
-            <Card
-              cardHeading={title}
-              cardBody={description}
-              image={cardImage}
-            />
-          </Grid>
-        );
-      })} */}
-    </Grid>
+          } = card;
+          console.log(card.node.title);
+          return (
+            <Grid item lg={3} xs={12} md={4} sm={6}>
+              <Card
+                cardHeading={title}
+                cardBody={description}
+                image={cardImage}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Layout>
   );
 };
-
-const MijnProjecten = () => (
-  <Layout>
-    <h1>Mijn projecten</h1>
-    <Cards />
-  </Layout>
-);
 
 export default MijnProjecten;
