038390a16e21c30379fe050c6a26c81cde0750e8
Jelle <jelle@fashionunited.com>
2021-06-03 23:10:12 +0200

    bladibla

--- web/src/pages/mijn-projecten.jsx
+++ web/src/pages/mijn-projecten.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
+import BlockContent from '@sanity/block-content-to-react';
 import { graphql } from 'gatsby';
 import Grid from '../components/Grid';
 import Layout from '../containers/Layout';
@@ -11,31 +12,24 @@ const Container = styled.div`
 `;
 
 export const projecPageQuery = graphql`
-  query projectPageQuery {
+  query {
     allSanityProjects {
-      edges {
-        node {
-          image1 {
-            asset {
-              gatsbyImageData(width: 500, height: 350, placeholder: BLURRED, formats: JPG)
-            }
-          }
-          title
-          description
-        }
+      nodes {
+        _rawDescription(resolveReferences: {maxDepth: 10})
       }
     }
   }
 `;
 
 const MijnProjecten = ({ data }) => {
-  const { allSanityProjects: { edges } } = data;
+  const { allSanityProjects: { nodes } } = data;
+  console.log(nodes);
   return (
     <Layout>
       <Container>
         <h1>Mijn projecten</h1>
-        <Grid container spacing={3}>
-          {edges.map((card) => {
+        {/* <Grid container spacing={3}>
+          {nodes.map((card) => {
             const {
               node: {
                 title,
@@ -50,6 +44,7 @@ const MijnProjecten = ({ data }) => {
 
             return (
               <Grid item lg={3} xs={12} md={4} sm={6}>
+                <BlockContent blocks={post._rawProjectDesc} serializers={serializers} />
                 <Card
                   cardHeading={title}
                   headingTag="h5"
@@ -59,7 +54,7 @@ const MijnProjecten = ({ data }) => {
               </Grid>
             );
           })}
-        </Grid>
+        </Grid> */}
       </Container>
     </Layout>
   );
